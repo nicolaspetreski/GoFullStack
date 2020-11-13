c2a9b33427d61d4d5716516c4b5bec1c3264498a
nick <n.petreski@live.com>
2020-11-13 04:42:40 -0800

    refactoring fix

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -15,40 +15,59 @@ exports.createSauce = (req, res, next) => { //données envoyé par le frontend s
         .catch(error => res.status(400).json({ error }));
 };
 
-exports.reactToSauce = (req, res, next) => { //Récupération de l'id de l'utilisateur et de la requête
-  if(req.body.like === 1){
-		Sauce.updateOne({ _id: req.params.id },  {$inc: {likes: req.body.like++} ,$push: {usersLiked: req.body.userId}})
-        .then ((sauce)=> res.status(200).json({ message: 'Liké avec succes !'}))
-        .catch(error => res.status(400).json({ error }));
-    } else if (req.body.like === -1){
-        Sauce.updateOne({ _id: req.params.id },  {$inc: {dislikes: (req.body.like++)*-1} ,$push: {usersDisliked: req.body.userId}})
-        .then ((sauce)=> res.status(200).json({ message: 'Disliké avec succes !'}))
-        .catch(error => res.status(400).json({ error }));
-    } else { 
-        
-		Sauce.findOne({_id: req.params.id})
-			.then(sauce => {
-				if (sauce.usersLiked.includes(req.body.userId)) {
-					Sauce.updateOne({_id: req.params.id}, {$pull: {usersLiked: req.body.userId}, $inc: {likes: -1}})
-                    	.then((sauce) => {res.status(200).json({ message: 'Un like de moins !'})}) 
-                    	.catch(error => res.status(400).json({ error }))
-                } else if (sauce.usersDisliked.includes(req.body.userId)) {
-                    Sauce.updateOne({_id: req.params.id}, {$pull: {usersDisliked: req.body.userId}, $inc: {dislikes: -1}})
-                    	.then((sauce) => {res.status(200).json({ message: 'Un dislike de moins !'})}) 
-                    	.catch(error => res.status(400).json({ error }))  
-                }
-            })
-            .catch(error => res.status(400).json({ error }));
-    }
+exports.getOneSauce = (req, res, next) => { // récupération d'une sauce
+	Sauce.findOne({_id: req.params.id})
+		.then((sauce) => {res.status(200).json(sauce);})
+		.catch(
+		(error) => {
+			res.status(404).json({
+				error: error
+			});
+		}
+	);
 };
 
-
-exports.getOneSauce = (req, res, next) => {
-  //Renvoit la sauce avec l'id correspondant
+exports.reactToSauce = (req, res, next) => {// Fonction de likes/dislikes
     Sauce.findOne({ _id: req.params.id })
-        .then(sauce => res.status(200).json(sauce)) 
-        .catch(error => res.status(404).json({ error })) 
-}
+        .then((sauce) => {
+            switch (req.body.like) {
+                case 0:// Si je like ou dislike plus
+                    if (sauce.usersLiked.indexOf(req.body.userId) !== -1) {// Si je suis quelqu'un qui likait
+                        sauce.usersLiked.splice(sauce.usersLiked.indexOf(req.body.userId), 1);// Je like plus
+                        sauce.likes--;
+                        break;                                    // Un like de moins
+                    }
+                    else if (sauce.usersDisliked.indexOf(req.body.userId) !== -1) {// Si je suis quelqu'un qui dislikait
+                        sauce.usersDisliked.splice(sauce.usersDisliked.indexOf(req.body.userId), 1);// Je dislike plus
+                        sauce.dislikes--;
+                        break;
+                    }
+                case -1:// Si je dislike
+                    if ((sauce.usersDisliked.indexOf(req.body.userId) === -1) && (sauce.usersLiked.indexOf(req.body.userId) === -1)) {// Si je ne fait parti des dislikers, ni des likers
+                        sauce.dislikes++;// On dislike
+                        sauce.usersDisliked.push(req.body.userId);
+                        break;
+                    }
+                case 1:// Si je like, meme logique à l'envers
+                    if ((sauce.usersDisliked.indexOf(req.body.userId) === -1) && (sauce.usersLiked.indexOf(req.body.userId) === -1)) {
+                        sauce.likes++;
+                        sauce.usersLiked.push(req.body.userId);
+                        break;}
+                        default: // Si aucune des 4 options précédentes n'est vraie, ce message d'erreur apparaîtra
+                    throw { error: "Impossible de modifier vos likes, merci de bien vouloir réessayer" };
+    }             
+            Sauce.updateOne({ _id: req.params.id }, {//Mise à jour des infos suite au switch qui a mis a jour les tableaux et les nombres de like/dislike
+                usersLiked: sauce.usersLiked,
+                usersDisliked: sauce.usersDisliked,
+                likes: sauce.likes,
+                dislikes: sauce.dislikes,
+                _id: req.params.id
+            })
+                .then(() => res.status(200).json({ message: 'Mis à jour' }))
+                .catch(error => res.status(400).json({ error }));
+        })
+        .catch((error) => res.status(400).json({ error }));
+};
 
 exports.modifySauce = (req, res, next) => {  //vérifie si req.file existe
 	const sauceObject = req.file ? //il existe : traitement de la nouvelle image
